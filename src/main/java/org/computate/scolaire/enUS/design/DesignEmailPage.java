71d04badcbd251885a1cd155f437f882c8e3978c
ctate <computate@computate.org>
2020-07-16 17:40:23 -0600

    Logging emails

--- src/main/java/org/computate/scolaire/enUS/design/DesignEmailPage.java
+++ src/main/java/org/computate/scolaire/enUS/design/DesignEmailPage.java
@@ -832,7 +832,7 @@ public class DesignEmailPage extends DesignEmailPageGen<DesignEmailGenPage> {
 //		message.setSubject(String.format("Enrollment of %s for the %s", schoolEnrollment.getChildCompleteName(), schoolEnrollment.getSeasonCompleteName()));
 		message.setSubject(emailSubject);
 
-		LOGGER.info("Email from: {}, to: {}", message.getFrom(), String.join("; ", message.getTo()));
+		LOGGER.info("Email from: " + message.getFrom() + " to " + String.join("; ", message.getTo()));
 		WorkerExecutor workerExecutor = siteContext.getWorkerExecutor();
 		workerExecutor.executeBlocking(
 			blockingCodeHandler -> {
